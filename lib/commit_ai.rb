3a0bf2f0538ae78c881ed908f768fbdc7a1d29c0
Ken Vitug <106139850+ken-narra@users.noreply.github.com>
2024-10-03 04:21:45 +0800

    Refactor codebase summary and git history analysis methods.

--- lib/commit_ai.rb
+++ lib/commit_ai.rb
@@ -5,38 +5,8 @@ class CommitAI
     @client = OpenAI::Client.new(access_token: ENV['OPENAI_ACCESS_TOKEN'])
   end
 
-  def analyze_codebase
-    codebase_summary = ""
-
-    files = Dir.glob("**/*").reject { |f| f =~ %r{/(node_modules|\.git|\.DS_Store)} }
-
-    files.each do |file|
-      next unless File.file?(file)
-
-      file_content = File.read(file)
-      codebase_summary += "File: #{file}\n"
-      codebase_summary += "Content Preview:\n#{file_content[0..500]}\n"
-      codebase_summary += "\n---\n"
-    end
-
-    codebase_summary
-  end
-
-  def analyze_git_history
-    git_history = `git log --pretty=format:"%h - %an, %ar: %s"`.strip
-    if git_history.empty?
-      "No commits found"
-    else
-      git_history
-    end
-  end
-
   def generate_commit_message(diff)
-    codebase_summary = analyze_codebase
-    git_history = analyze_git_history
     prompt = <<~PROMPT
-      This is the codebase summary: #{codebase_summary}
-      This is the git history: #{git_history}
       This is the git diff: #{diff}
 
       Please generate a **single-line** commit message that is concise and follows best practices.
